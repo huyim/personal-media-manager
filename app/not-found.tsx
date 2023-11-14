13c0cde0a0512d00299212e86ecc2e8c4b7759cf
huyim <huym0625@gmail.com>
2023-11-14 02:27:51 +0100

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,9 @@ import { Boundary } from '#/ui/boundary';
 
 export default function NotFound() {
   return (
-    <Boundary labels={['not-found.tsx']} color="pink">
+    <Boundary labels={['PAGE NOT FOUND']} color="pink">
       <div className="text-vercel-pink space-y-4">
-        <h2 className="text-lg font-bold">Not Found</h2>
+        <h2 className="text-lg font-bold">404 Not Found</h2>
 
         <p className="text-sm">Could not find requested resource</p>
       </div>
