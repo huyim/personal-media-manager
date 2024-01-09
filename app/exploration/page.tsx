1de7d24ad310987d4843ed6e1985e56bc5d589b3
huyim <huym0625@gmail.com>
2024-01-09 13:48:29 +0100

    big update

--- app/exploration/page.tsx
+++ app/exploration/page.tsx
@@ -3,25 +3,36 @@
 import type { NextPage } from 'next';
 import MediaGraph from '../../components/MediaGraph';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home: NextPage = () => {
+  // const [input, setInput] = useState();
+
+  // const handleChange = (event: any) => {
+  //   setInput(event.target.value);
+  // };
+
   return (
     <div className="prose prose-sm prose-invert max-w-none">
-      <h1 className="text-center text-xl font-bold text-black">
-        Explore my database
-      </h1>
-      <input
-        type="search"
-        id="default-search"
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-        placeholder="Search tags..."
-        required
-      />
-      <br />
-      {/* <div className="mx-auto w-full max-w-3xl px-3"> */}
-      <MediaGraph />
-      {/* </div> */}
+      <div style={{ position: 'fixed', width: '55%', height: '75%' }}>
+        <h1 className="text-center text-xl font-bold text-black">
+          Explore my database
+        </h1>
+        {/* <input
+          type="search"
+          id="search"
+          value={input}
+          onChange={handleChange}
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="Search tags..."
+          required
+        />
+        <br /> */}
+
+        {/* <div className="mx-auto w-full max-w-3xl px-3"> */}
+        <MediaGraph />
+        {/* </div> */}
+      </div>
     </div>
   );
 };
--- components/MediaGraph.tsx
+++ components/MediaGraph.tsx
@@ -8,13 +8,13 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-import CustomNode from './CustomNode';
+import VideoNode from './VideoNode';
 
 const initBgColor = '#FFFFFF';
 
-const connectionLineStyle = { stroke: '#fff' };
+const connectionLineStyle = { stroke: '#000' };
 const nodeTypes = {
-  selectorNode: CustomNode,
+  videoNode: VideoNode,
 };
 
 const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
@@ -23,6 +23,11 @@ const MediaGraph = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const [bgColor, setBgColor] = useState(initBgColor);
+  const [input, setInput] = useState();
+
+  const handleChange = (event: any) => {
+    setInput(event.target.value);
+  };
 
   useEffect(() => {
     const onChange = (event: any) => {
@@ -51,15 +56,26 @@ const MediaGraph = () => {
       {
         id: '1',
         type: 'input',
-        data: { label: 'Some labels' },
+        data: { label: 'Zurich' },
         position: { x: 0, y: 50 },
       },
       {
         id: '2',
-        type: 'selectorNode',
-        data: { onChange: onChange, color: initBgColor },
-        style: { border: '1px solid #777', padding: 10 },
+        type: 'videoNode',
+        data: {
+          onChange: onChange,
+          url: 'http://localhost:8080/v96cgPsS2EDdb5eNhd9eYwAk3dB5r2BgfeW696kbrF48',
+        },
         position: { x: -100, y: 100 },
+      },
+      {
+        id: '3',
+        type: 'videoNode',
+        data: {
+          onChange: onChange,
+          url: '<http://localhost:8080/v96cgPsS2EDdb5eNhd9eYwAk3dB5r2BgfeW696kbrF48/c/12li--VEhzOhVAFIc2DV2g>',
+        },
+        position: { x: 120, y: 100 },
       },
     ]);
 
@@ -69,15 +85,14 @@ const MediaGraph = () => {
         source: '1',
         target: '2',
         animated: true,
-        style: { stroke: '#fff' },
+        style: { stroke: '#000' },
       },
       {
         id: 'e2a-3',
-        source: '2',
+        source: '1',
         target: '3',
-        sourceHandle: 'a',
         animated: true,
-        style: { stroke: '#fff' },
+        style: { stroke: '#000' },
       },
       {
         id: 'e2b-4',
@@ -85,7 +100,7 @@ const MediaGraph = () => {
         target: '4',
         sourceHandle: 'b',
         animated: true,
-        style: { stroke: '#fff' },
+        style: { stroke: '#000' },
       },
     ]);
   }, []);
@@ -99,22 +114,36 @@ const MediaGraph = () => {
   );
   return (
     <div style={{ position: 'fixed', width: '55%', height: '75%' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        style={{ background: bgColor }}
-        nodeTypes={nodeTypes}
-        connectionLineStyle={connectionLineStyle}
-        defaultViewport={defaultViewport}
-        fitView
-        attributionPosition="bottom-left"
-      >
-        <MiniMap />
-        <Controls />
-      </ReactFlow>
+      <input
+        type="search"
+        id="search"
+        value={input}
+        onChange={handleChange}
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        placeholder="Search tags..."
+        required
+      />
+      <br />
+      {input ? (
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          style={{ background: bgColor }}
+          nodeTypes={nodeTypes}
+          connectionLineStyle={connectionLineStyle}
+          defaultViewport={defaultViewport}
+          fitView
+          attributionPosition="bottom-left"
+        >
+          <MiniMap />
+          <Controls />
+        </ReactFlow>
+      ) : (
+        <div>Search tags to get graph...</div>
+      )}
     </div>
   );
 };
